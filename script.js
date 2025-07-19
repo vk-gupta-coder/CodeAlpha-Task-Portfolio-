6053f7f92157acd4e36556d1c54ee5ee2c41544a
Vartika Gupta <vartikag522@gmail.com>
2025-07-19 18:44:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.2,
+  rootMargin: '0px 0px -50px 0px'
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('appear');
+    observer.unobserve(entry.target);
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
